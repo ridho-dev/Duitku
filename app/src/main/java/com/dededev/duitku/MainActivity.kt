f3a6348bd8e9a7f4e05e88b0ced7570bdee2f67e
ridho-dev <ridhopardede.dev@gmail.com>
2022-05-03 11:02:12 +0700

    Initial commit

---
+++ app/src/main/java/com/dededev/duitku/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dededev.duitku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
