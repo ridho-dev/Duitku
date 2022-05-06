9f7082c3192832b5d5325a3a8f36c8a6c6caef8b
ridho-dev <ridhopardede.dev@gmail.com>
2022-05-06 11:33:22 +0700

    Add new activity: AddTransactionActivity.kt

---
+++ app/src/main/java/com/dededev/duitku/ui/addTransaction/AddTransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.dededev.duitku.ui.addTransaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dededev.duitku.R
+
+class AddTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_transaction)
+    }
+}
